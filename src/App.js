3c6daa601cd361d184c19b47169e8f873d3fca15
Tatiana Moreira <tatimb14@gmail.com>
2018-09-18 20:28:47 -0600

    Adding style with styled-components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  background-color: #EEEEEE;
+`
+const TodoList = styled.div`
+  background-color: #FFF;
+  padding: 20px 20px 10px 20px;
+  width: 600px;
+  margin: 30px auto;
+`
+const TodoListH1 = styled.h1`
+  margin: 0;
+  padding-bottom: 20px;
+  text-align: center;
+  color : #824C71;
+`
+const TodoListItems = styled.li`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em 0.2em 0;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`
+const TodoListContainer = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
 
 function App() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="todolist">
-                    <h1>Todos</h1>
-                    <ul className="list-unstyled">
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Learn React</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Learn Angular</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Learn Nodejs</label>
-                            </div>
-                        </li>
-                    </ul>
-
-                </div>
+  return (
+    <AppWrapper>
+      <TodoList>
+        <TodoListH1>Todos</TodoListH1>
+        <TodoListContainer>
+          <TodoListItems>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" value="" />Learn React</label>
             </div>
-        </div>
-    );
+          </TodoListItems>
+          <TodoListItems>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" value="" />Learn Angular</label>
+            </div>
+          </TodoListItems>
+          <TodoListItems>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" value="" />Learn Nodejs</label>
+            </div>
+          </TodoListItems>
+        </TodoListContainer>
+
+      </TodoList>
+
+    </AppWrapper>
+  );
 }
 
 export default App;
