8d1d0b2f3c5c82ba82bd253f97bf985171937b07
Tatiana Moreira <tatimb14@gmail.com>
2018-09-18 18:55:30 -0600

    Adding basic structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container">
+            <div className="row">
+                <div className="todolist">
+                    <h1>Todos</h1>
+                    <ul className="list-unstyled">
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Learn React</label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Learn Angular</label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Learn Nodejs</label>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
     );
-  }
 }
 
 export default App;
